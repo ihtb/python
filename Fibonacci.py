be10f0a5698a0b54b9f818c5c1363d68e5c06767
ihtb <unyuband@yahoo.co.jp>
2017-01-08 17:56:18 +0900

    add Fibonacci.py. use python Fibonacci.py [num]

--- Fibonacci.py
+++ Fibonacci.py
@@ -5,12 +5,10 @@ input = sys.argv
 print input[1]
 
 flist = [0, 1]
-i = 2
+i = 1
 
-while i < 10:
-	flist.append(flist[i - 1] + flist[i - 2])
-	if flist[i] > input[1]:
-		break
+while flist[i] < int(input[1]):
+	flist.append(flist[i] + flist[i-1])
 	i += 1
 
 print flist

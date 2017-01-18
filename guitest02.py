63d83e38c7465901615c00ce5ba5d4e73016361e
daiichikobo <daiichikobo@daiichikobonoiMac.local>
2017-01-18 11:31:25 +0900

    set boxes which have backgroundcolor.

--- guitest02.py
+++ guitest02.py
@@ -4,7 +4,18 @@ import wx
 
 application = wx.App()
 
-frame = wx.Frame(None, wx.ID_ANY, u"テストフレーム")
+frame = wx.Frame(None, wx.ID_ANY, u"テストフレーム", size=(300,300), pos=(0,100))
+frame.SetBackgroundColour("#000000")
+
+r_panel = wx.Panel(frame, wx.ID_ANY, pos=(0,0), size=(80,300))
+r_panel.SetBackgroundColour("#00ff00")
+
+r_panel = wx.Panel(frame, wx.ID_ANY, pos=(80,0), size=(80,300))
+r_panel.SetBackgroundColour("#0000ff")
+
+r_panel = wx.Panel(frame, wx.ID_ANY, pos=(160,0), size=(80,300))
+r_panel.SetBackgroundColour("#ff0000")
+
 frame.Show()
 
 application.MainLoop()

cda4e8bb946b458bdc980fd2735ec73cfd6d4a87
ihtb <unyuband@yahoo.co.jp>
2017-02-08 21:39:04 +0900

    multi panel test

---
+++ guitest07.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+import wx
+
+#******************************
+# Rootパネルクラス
+# ******************************
+class Root_class:
+    def __init__(self, None, wx_ID_ANY)
+
+#******************************
+# アプリケーション起動
+# ******************************
+if __name__ == "__main__"
+    applicaiton = wx.App()
+    frame = Root_frame()
+    frame.Show()
+    application.MainLoop()

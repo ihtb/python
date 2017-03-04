2f3506fdc9cbe87f50da1d0583c7c7ad43ffcd8e
daiichikobo <daiichikobo@daiichikobonoiMac.local>
2017-03-04 20:27:53 +0900

    畳み込みの計算

---
+++ test32.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+test_num_list = [0, 1, 2, 3]
+print("イテレート前のリスト : ", end="")
+print(test_num_list)
+for i in test_num_list:
+    i = 0
+print("イテレート後のリスト : ", end="")
+print(test_num_list)
---
+++ test33.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+i_list = [0, 1, 2, 3, 5, 7, 11]  #入力
+g_list = [32, 16, 8, 4, 2, 1, 0] #インパルス応答
+o_list = [0, 0, 0, 0, 0, 0, 0]   #出力
+
+for i in range(len(i_list)):
+    for j in range(i+1):
+        o_list[i] += i_list[j] * g_list[i-j]
+
+print("入力 : ", end="")
+print(i_list)
+print("インパルス応答 : ", end="")
+print(g_list)
+print("出力 : ", end="")
+print(o_list)
